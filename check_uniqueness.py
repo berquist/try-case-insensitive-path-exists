f0659c6fbeb9de8a7656ceb5b411830e7ab54411
Eric Berquist <eric.berquist@gmail.com>
2024-06-09 15:52:26 -0400

    time more uniqueness checking

--- check_uniqueness.py
+++ check_uniqueness.py
@@ -10,6 +10,22 @@ from tempfile import NamedTemporaryFile, TemporaryDirectory
 #
 # %timeit is_fs_case_sensitive()
 # 97.7 ns ± 0.626 ns per loop (mean ± std. dev. of 7 runs, 10,000,000 loops each)
+
+# darwin
+# CPU times: user 24 μs, sys: 51 μs, total: 75 μs
+# Wall time: 77 μs
+# CPU times: user 1 μs, sys: 0 ns, total: 1 μs
+# Wall time: 1.67 μs
+# linux
+# CPU times: user 97 μs, sys: 11 μs, total: 108 μs
+# Wall time: 112 μs
+# CPU times: user 3 μs, sys: 0 ns, total: 3 μs
+# Wall time: 5.48 μs
+# win32 (???)
+# CPU times: total: 0 ns
+# Wall time: 0 ns
+# CPU times: total: 0 ns
+# Wall time: 0 ns
 def is_fs_case_sensitive(source_dir=None):
     if not hasattr(is_fs_case_sensitive, "case_sensitive"):
         with NamedTemporaryFile(prefix="TmP", dir=source_dir) as tmp_file:
@@ -21,30 +37,29 @@ def is_fs_case_sensitive(source_dir=None):
     return is_fs_case_sensitive.case_sensitive
 
 
-# %timeit assert_unique_1(fn_base, stub_upper)
-# 9.41 μs ± 32.3 ns per loop (mean ± std. dev. of 7 runs, 100,000 loops each)
-def assert_unique_1(parent_path: Path, name: str) -> bool:
+def is_unique_1(parent_path: Path, name: str) -> bool:
     try:
         str(name)
     except UnicodeEncodeError:
         name = name.encode("utf-8")
 
     if (parent_path / name).exists():
-        # raise RuntimeError(f"{name}' already exists in '{parent_path}")
         return True
     return False
 
 
-# %timeit assert_unique_2(fn_base, stub_upper, stub_lower)
-# 331 μs ± 1.11 μs per loop (mean ± std. dev. of 7 runs, 1,000 loops each)
-def assert_unique_2(parent_path: Path, name: str, name_lower: str) -> bool:
+def is_unique_2_pathlib(parent_path: Path, name_lower: str) -> bool:
     # os.listdir is much faster here than os.walk or parent_path.iterdir
     for item in os.listdir(str(parent_path)):
         if name_lower == item.lower():
-            # raise RuntimeError(
-            #     f"A directory with name (case independent) '{name}' already exists"
-            #     " and cannot be made according to the naming rule 'thorough'."
-            # )
+            return True
+    return False
+
+
+def is_unique_2(parent_path: str, name_lower: str) -> bool:
+    # os.listdir is much faster here than os.walk or parent_path.iterdir
+    for item in os.listdir(parent_path):
+        if name_lower == item.lower():
             return True
     return False
 
@@ -139,3 +154,7 @@ if __name__ == "__main__":
         # darwin: false true true
         # linux: true false true
         # win32: false true true
+        # These should also be platform-independent.
+        assert not is_unique_1(fn_base, stub_lower)
+        assert not is_unique_2_pathlib(fn_base, stub_lower)
+        assert not is_unique_2(temp_dir, stub_lower)
